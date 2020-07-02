cad01d38d6f3b5a9d2d3bcfd823ac55a6e76637f
marbilher <marbilher@MBH-MACBOOK.local>
2020-07-02 16:10:56 -0700

    initial commit

---
+++ BoardModel/Board.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace BoardModel
+{
+    public class Board
+    {
+        public int Size { get; set; }
+        public Cell[,] cellGrid { get; set; }
+
+        public Board (int s)
+        {
+            Size = s;
+
+            cellGrid = new Cell[Size, Size];
+
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    cellGrid[i, j] = new Cell(i, j);
+                }
+            }
+
+        }
+    }
+}
---
+++ BoardModel/Cell.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace BoardModel
+{
+    public class Cell
+    {
+        public int RowNumber { get; set; }
+        public int ColumnNumber { get; set; }
+        public bool CurrentlyOccupied { get; set; }
+        public bool LegalNextMove { get; set; }
+
+        public Cell(int x, int y)
+        {
+            RowNumber = x;
+            ColumnNumber = y;
+        }
+    }
+}
