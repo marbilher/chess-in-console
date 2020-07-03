b878358d94a9bf110b145fdb97978d641f0bff93
marbilher <marbilher@MBH-MACBOOK.local>
2020-07-03 11:33:52 -0700

    knight movement

---
+++ Chess/Board.cs
@@ -0,0 +1,76 @@
+﻿using System;
+namespace BoardModel
+{
+    public class Board
+    {
+        public int Size { get; set; }
+        public Cell[,] cellGrid { get; set; }
+
+        public Board (int s)
+        {
+            Size = s;
+
+            cellGrid = new Cell[Size, Size];
+
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    cellGrid[i, j] = new Cell(i, j);
+                }
+            }
+
+        }
+
+        public void MarkNextLegalMoves( Cell currentCell, string chessPiece)
+        {
+            //Clear all previous legal moves
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    cellGrid[i, j].LegalNextMove = false;
+                    cellGrid[i, j].CurrentlyOccupied = false;
+                }
+            }
+
+            //Find all legal moves and mark cells as legal
+
+            switch (chessPiece)
+            {
+                case "King":
+                    break;
+
+                case "Queen":
+                    break;
+
+                case "Bishop":
+                    break;
+
+                case "Rook":
+                    break;
+
+                case "Knight":
+                    cellGrid[currentCell.RowNumber + 2, currentCell.RowNumber + 1].LegalNextMove = true;
+                    cellGrid[currentCell.RowNumber + 2, currentCell.RowNumber - 1].LegalNextMove = true;
+                    cellGrid[currentCell.RowNumber - 2, currentCell.RowNumber + 1].LegalNextMove = true;
+                    cellGrid[currentCell.RowNumber - 2, currentCell.RowNumber - 1].LegalNextMove = true;
+                    cellGrid[currentCell.RowNumber + 1, currentCell.RowNumber - 2].LegalNextMove = true;
+                    cellGrid[currentCell.RowNumber + 1, currentCell.RowNumber + 2].LegalNextMove = true;
+                    cellGrid[currentCell.RowNumber - 1, currentCell.RowNumber - 2].LegalNextMove = true;
+                    cellGrid[currentCell.RowNumber - 1, currentCell.RowNumber + 2].LegalNextMove = true;
+
+
+                    break;
+
+                case "Pawn":
+                    break;
+
+                default:
+
+                    break;
+
+            }
+        }
+    }
+}
--- Chess/Program.cs
+++ Chess/Program.cs
@@ -1,12 +1,62 @@
 ﻿using System;
+using BoardModel;
 
 namespace Chess
 {
     class Program
     {
+        static Board myBoard = new Board(8);
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //show empty board
+            printBoard(myBoard);
+
+            //ask user for x and y coords
+            Cell currentCell = setCurrentCell();
+            currentCell.CurrentlyOccupied = true;
+            //calculate all legal moves for that piece
+            myBoard.MarkNextLegalMoves(currentCell, "Knight");
+
+            // print the board
+            printBoard(myBoard);
+
+            //wait for key press before ending program
+            Console.ReadLine();
+        }
+
+        private static Cell setCurrentCell()
+        {
+            //get x and y from user, return cell location
+            Console.WriteLine("Enter the current row number:");
+            int currentRow = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the current column number:");
+            int currentColumn = int.Parse(Console.ReadLine());
+
+            return myBoard.cellGrid[currentRow, currentColumn];
+        }
+
+        private static void printBoard(Board myBoard)
+        {
+            //print the chess board to console
+            for (int i = 0; i < myBoard.Size; i++)
+            {
+                for (int j = 0; j < myBoard.Size; j++)
+                {
+                    Cell c = myBoard.cellGrid[i, j];
+
+                    if(c.CurrentlyOccupied)
+                    {
+                        Console.Write('X');
+                    } else if (c.LegalNextMove){
+                        Console.Write('+');
+                    } else
+                    {
+                        Console.Write(".");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("======================");
         }
     }
 }
